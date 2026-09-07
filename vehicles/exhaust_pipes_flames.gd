23a3357921ee5fbf6e9373abc4997a77416aab97
Daniel John Hefferman <117324351+smoothbrainsinc@users.noreply.github.com>
2026-09-07 16:43:00 -0500

    nuns

--- vehicles/exhaust_pipes_flames.gd
+++ vehicles/exhaust_pipes_flames.gd
@@ -6,8 +6,6 @@ extends Node3D
 @export var v_frames: int = 5
 @export var flame_scale: float = 0.5
 @export var exhaust_direction: Vector3 = Vector3(0, 1, 0)
-@export var amount: int = 16
-@export var lifetime: float = 0.3
 
 
 func _ready():
@@ -27,7 +25,7 @@ func setup_flame(marker: Node3D):
 	particles.name = "ExhaustFlame"
 	particles.amount = 16
 	particles.lifetime = 0.3
-	particles.local_coords = false
+	particles.local_coords = true
 	particles.emitting = true
 	
 	# Simulation properties
