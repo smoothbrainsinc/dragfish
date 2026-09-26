2432cba40b846a7b7892f05f33f3095c5b247aa4
Daniel John Hefferman <117324351+smoothbrainsinc@users.noreply.github.com>
2026-09-26 03:38:21 -0500

    finally watergit add .!

--- Lakes/hell_lake/new_hell_awaits_water.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var water_mat: ShaderMaterial = $WaterMesh.get_surface_override_material(0)
-@onready var capture_cam: Camera3D = $SubViewport/TopDownCamera
-
-func _process(_delta):
-	water_mat.set_shader_parameter("cam_position", capture_cam.global_position)
-	water_mat.set_shader_parameter("orthographic_cam_size", capture_cam.size)
-	water_mat.set_shader_parameter("render_texture", $SubViewport.get_texture())
